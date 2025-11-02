026ea5157b5b83690f18b7778df25ecf6ecfbd71
Jérôme Naucelle <Jerome.Naucelle@hsc-labs.com>
2025-11-02 10:30:37 +0100

    corrections sur la forme suite retours de Chloé

--- django_app/core/form.py
+++ django_app/core/form.py
@@ -6,7 +6,3 @@ class RegisterForm(RegistrationForm):
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name", "password1", "password2", "email"]
-        labels = {
-            'first_name': 'Prénom (réel !)',
-            'last_name': 'Nom (réel !)',
-        }
